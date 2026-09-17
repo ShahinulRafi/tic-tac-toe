7949b672eddc4a5760ff480f6c89bb7504dd2107
ShahinulRafi <shahinul.csecu@gmail.com>
2026-09-18 00:32:42 +0600

    implementaing square and board

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello Tic Tac Toe</h1>;
+function Square() {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+      X
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    //don't write unnecessary div, use fragment instead
+    <>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
